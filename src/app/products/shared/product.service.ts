c3cd83b1f398e680cabdd98f1b577b58dd089a0f
Troels Klein <trk@trifork.com>
2023-03-06 13:53:01 +0100

    Added http client to product service

---
+++ src/app/products/shared/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { ProductDto } from 'src/app/shared/dtos/product.dto';
+import {environment} from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private _http: HttpClient) {}
+
+  getAll(): Observable<ProductDto[]> {
+    return this._http
+      .get<ProductDto[]>(environment + '/api/Product');
+  }
+}
